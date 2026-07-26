d072d3879ed52f28459fd502fb9663db3e973530
Corbino5021 <132967304+Corbino50211@users.noreply.github.com>
2026-07-26 13:18:33 -0500

    Add generated physical humanoid descriptor

---
+++ Packages/com.pancakedevs.apexphysics/Runtime/Characters/Humanoid/ApexPhysicalHumanoid.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+namespace PancakeDevs.ApexPhysics
+{
+    /// <summary>
+    /// Stores the generated pieces of an Apex physical humanoid conversion.
+    /// The animated target drives a visible physical clone through ApexActiveRagdoll.
+    /// </summary>
+    [DisallowMultipleComponent]
+    public sealed class ApexPhysicalHumanoid : MonoBehaviour
+    {
+        [SerializeField] private ApexPhysicalHumanoidMode mode;
+
+        [Header("Generated Hierarchies")]
+        [SerializeField] private GameObject animatedTargetCharacter;
+        [SerializeField] private GameObject physicalCharacter;
+        [SerializeField] private Animator targetAnimator;
+        [SerializeField] private Transform targetHips;
+        [SerializeField] private ApexRagdollBone physicalHips;
+
+        [Header("Apex Systems")]
+        [SerializeField] private ApexActiveRagdoll activeRagdoll;
+        [SerializeField] private ApexHumanoidTargetRootDriver targetRootDriver;
+        [SerializeField] private ApexHumanoidTrackingDriver trackingDriver;
+        [SerializeField] private ApexRagdollCollisionFilter collisionFilter;
+        [SerializeField] private ApexPhysicalPlayerRig playerRig;
+        [SerializeField] private ApexNPCNavigator npcNavigator;
+        [SerializeField] private ApexNPCMotor npcMotor;
+        [SerializeField] private ApexNPCBrain npcBrain;
+
+        public ApexPhysicalHumanoidMode Mode => mode;
+        public GameObject AnimatedTargetCharacter => animatedTargetCharacter;
+        public GameObject PhysicalCharacter => physicalCharacter;
+        public Animator TargetAnimator => targetAnimator;
+        public Transform TargetHips => targetHips;
+        public ApexRagdollBone PhysicalHips => physicalHips;
+        public ApexActiveRagdoll ActiveRagdoll => activeRagdoll;
+        public ApexHumanoidTargetRootDriver TargetRootDriver => targetRootDriver;
+        public ApexHumanoidTrackingDriver TrackingDriver => trackingDriver;
+        public ApexRagdollCollisionFilter CollisionFilter => collisionFilter;
+        public ApexPhysicalPlayerRig PlayerRig => playerRig;
+        public ApexNPCNavigator NPCNavigator => npcNavigator;
+        public ApexNPCMotor NPCMotor => npcMotor;
+        public ApexNPCBrain NPCBrain => npcBrain;
+
+        public void Configure(
+            ApexPhysicalHumanoidMode newMode,
+            GameObject newAnimatedTargetCharacter,
+            GameObject newPhysicalCharacter,
+            Animator newTargetAnimator,
+            Transform newTargetHips,
+            ApexRagdollBone newPhysicalHips,
+            ApexActiveRagdoll newActiveRagdoll,
+            ApexHumanoidTargetRootDriver newTargetRootDriver,
+            ApexHumanoidTrackingDriver newTrackingDriver,
+            ApexRagdollCollisionFilter newCollisionFilter,
+            ApexPhysicalPlayerRig newPlayerRig,
+            ApexNPCNavigator newNpcNavigator,
+            ApexNPCMotor newNpcMotor,
+            ApexNPCBrain newNpcBrain)
+        {
+            mode = newMode;
+            animatedTargetCharacter = newAnimatedTargetCharacter;
+            physicalCharacter = newPhysicalCharacter;
+            targetAnimator = newTargetAnimator;
+            targetHips = newTargetHips;
+            physicalHips = newPhysicalHips;
+            activeRagdoll = newActiveRagdoll;
+            targetRootDriver = newTargetRootDriver;
+            trackingDriver = newTrackingDriver;
+            collisionFilter = newCollisionFilter;
+            playerRig = newPlayerRig;
+            npcNavigator = newNpcNavigator;
+            npcMotor = newNpcMotor;
+            npcBrain = newNpcBrain;
+        }
+    }
+}
