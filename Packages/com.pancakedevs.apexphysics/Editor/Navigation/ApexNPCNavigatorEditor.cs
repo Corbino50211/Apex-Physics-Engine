027027ed4ede9451861fbda84aa6f7a8e4b4a09c
Corbino5021 <132967304+Corbino50211@users.noreply.github.com>
2026-07-26 04:01:16 -0500

    Add NPC behavior repair and diagnostics

--- Packages/com.pancakedevs.apexphysics/Editor/Navigation/ApexNPCNavigatorEditor.cs
+++ Packages/com.pancakedevs.apexphysics/Editor/Navigation/ApexNPCNavigatorEditor.cs
@@ -13,6 +13,7 @@ namespace PancakeDevs.ApexPhysics.Editor
 
             ApexNPCNavigator navigator = (ApexNPCNavigator)target;
             ApexNPCMotor motor = navigator.GetComponent<ApexNPCMotor>();
+            ApexNPCBrain brain = navigator.GetComponent<ApexNPCBrain>();
 
             EditorGUILayout.Space();
             EditorGUILayout.LabelField("Apex NPC Navigation", EditorStyles.boldLabel);
@@ -31,13 +32,27 @@ namespace PancakeDevs.ApexPhysics.Editor
                 }
             }
 
+            if (brain == null)
+            {
+                EditorGUILayout.HelpBox(
+                    "This NPC can move to assigned destinations, but it cannot wander or react to hard impacts because ApexNPCBrain is missing.",
+                    MessageType.Warning);
+
+                if (GUILayout.Button("Add NPC Behavior Brain"))
+                {
+                    Undo.AddComponent<ApexNPCBrain>(navigator.gameObject);
+                    EditorUtility.SetDirty(navigator.gameObject);
+                    return;
+                }
+            }
+
             if (!Application.isPlaying)
             {
                 EditorGUILayout.HelpBox(
-                    motor != null
-                        ? "This NPC has a physical motor. Enter Play Mode to test movement and inspect live path data."
-                        : "Add the physical motor above, then enter Play Mode.",
-                    motor != null ? MessageType.Info : MessageType.Warning);
+                    motor != null && brain != null
+                        ? "This NPC has physical movement and behavior. Enter Play Mode to test wandering, chasing, and live path data."
+                        : "Add the missing NPC components above, then enter Play Mode.",
+                    motor != null && brain != null ? MessageType.Info : MessageType.Warning);
                 return;
             }
 
@@ -62,6 +77,15 @@ namespace PancakeDevs.ApexPhysics.Editor
                 {
                     EditorGUILayout.Vector3Field("Rigidbody Velocity", rigidbody.velocity);
                 }
+            }
+
+            if (brain != null)
+            {
+                EditorGUILayout.Space();
+                EditorGUILayout.LabelField("Behavior", EditorStyles.boldLabel);
+                EditorGUILayout.LabelField("Current Mode", brain.CurrentMode.ToString());
+                EditorGUILayout.ObjectField("Chase Target", brain.ChaseTarget, typeof(Transform), true);
+                EditorGUILayout.FloatField("Chase Time Remaining", brain.RemainingImpactChaseTime);
             }
 
             using (new EditorGUILayout.HorizontalScope())
