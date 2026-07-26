0d2f56a2ad4b35022887914ec063b62d2158d535
Corbino5021 <132967304+Corbino50211@users.noreply.github.com>
2026-07-26 03:25:37 -0500

    Add Apex handedness

---
+++ Packages/com.pancakedevs.apexphysics/Runtime/Interaction/ApexHandedness.cs
@@ -0,0 +1,10 @@
+namespace PancakeDevs.ApexPhysics
+{
+    /// <summary>Identifies which hand may use a grabber or grab point.</summary>
+    public enum ApexHandedness
+    {
+        Any = 0,
+        Left = 1,
+        Right = 2
+    }
+}
